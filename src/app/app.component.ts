adfe004ea68f4ac84c2393f4899bc3aa42bf173f
Taneja <TanejaV@haufe.com>
2020-10-12 09:13:21 +0530

    insta clone listing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'instagram-clone-hackerearth';
+  cards = [];
+
+  constructor(private dataService: DataService) { }
+
+
+  ngOnInit() {
+
+    this.dataService.getInstagramData().subscribe((data: any[]) => {
+      console.log(data);
+      this.cards = data;
+    });
+  }
+
+  sortByLike(){
+    this.cards.sort(this.compare);
+  }
+
+  compare(a, b){
+    if (a.likes < b.likes){
+      return -1;
+    }
+    if(a.likes > b.likes){
+      return 1;
+    }
+    return 0;
+  }
+
+  sortByTime(){
+    this.cards.sort((a, b) => new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime());
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+
+import { HttpClientModule } from '@angular/common/http';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
